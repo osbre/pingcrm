8509e807a157e454c31c14cf78ac8186f9d814cf
Troy Martin <troy@devato.com>
2020-02-23 11:49:32 -0500

    Disable auto seed

--- test/ping/accounts/user_search_test.exs
+++ test/ping/accounts/user_search_test.exs
@@ -1,33 +1,19 @@
-# defmodule Ping.Accounts.UserSearchTest do
-#   use Ping.DataCase
+defmodule Ping.Accounts.UserSearchTest do
+  use Ping.DataCase
 
-#   alias Ping.Accounts.{UserSearch}
+  alias Ping.Accounts.{UserSearch}
 
-#   defp users_for_search(_context) do
-#     # Invoked in every block in "a block"
-#     {:ok, name: "John", age: 54}
-#   end
+  defp users_for_search(_context) do
+    # Invoked in every block in "a block"
+    {:ok, name: "John", age: 54}
+  end
 
-#   describe "UserSearch no params" do
-#     setup [:users_for_search]
+  describe "UserSearch no params" do
+    setup [:users_for_search]
 
-#     test "search/2 with valid data finds users" do
-#       results = UserSearch.search(%{}, 0)
-#       assert length(results) == 2
-#     end
-#   end
-# end
-
-# # defp my_hook(_context) do
-# #   # Invoked in every block in "a block"
-# #   {:ok, name: "John", age: 54}
-# # end
-
-# # describe "a block" do
-# #   setup [:my_hook]
-
-# #   test "John's age", context do
-# #     assert context[:name] == "John"
-# #     assert context[:age] == 54
-# #   end
-# # end
+    test "search/2 with valid data finds users" do
+      results = UserSearch.search(%{}, 0)
+      assert length(results) == 2
+    end
+  end
+end

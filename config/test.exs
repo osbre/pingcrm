13bfd187877678e91db6dc394646b30c6169d2a1
Troy Martin <troy@devato.com>
2020-02-22 22:00:07 -0500

    Update db config

--- config/test.exs
+++ config/test.exs
@@ -5,8 +5,15 @@ config :ping, Ping.Repo,
   username: "postgres",
   password: "postgres",
   database: "ping_test",
-  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+# Configure the database for GitHub Actions
+if System.get_env("GITHUB_ACTIONS") do
+  config :ping, Ping.Repo,
+    hostname: "postgres",
+    username: "postgres",
+    password: "postgres"
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.

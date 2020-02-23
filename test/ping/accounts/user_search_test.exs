a36478390351f42a358ef0fe2aa1436c49f0477d
Troy Martin <troy@devato.com>
2020-02-23 09:30:23 -0500

    Update tests

---
+++ test/ping/accounts/user_search_test.exs
@@ -0,0 +1,34 @@
+defmodule Ping.Accounts.UserSearchTest do
+  # use Ping.DataCase
+
+  # alias Ping.Accounts
+
+  # describe "users" do
+  #   alias Hello.Accounts.User
+
+  #   @valid_attrs %{bio: "some bio", email: "some email", name: "some name", number_of_pets: 42}
+  #   @update_attrs %{
+  #     bio: "some updated bio",
+  #     email: "some updated email",
+  #     name: "some updated name",
+  #     number_of_pets: 43
+  #   }
+  #   @invalid_attrs %{bio: nil, email: nil, name: nil, number_of_pets: nil}
+
+  #   # ...
+
+  #   test "create_user/1 with valid data creates a user" do
+  #     assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
+  #     assert user.bio == "some bio"
+  #     assert user.email == "some email"
+  #     assert user.name == "some name"
+  #     assert user.number_of_pets == 42
+  #   end
+
+  #   test "create_user/1 with invalid data returns error changeset" do
+  #     assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
+  #   end
+
+  #   # ...
+  # end
+end

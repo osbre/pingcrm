ea27d7575e204d06372be282d489cfc77434024f
Troy Martin <troy@devato.com>
2020-02-23 19:38:52 -0500

    Update error view tests

--- lib/ping_web/views/error_view.ex
+++ lib/ping_web/views/error_view.ex
@@ -1,7 +1,9 @@
 defmodule PingWeb.ErrorView do
   use PingWeb, :view
 
-  def render("500.html", _assigns), do: "500.html"
+  def render("500.html", _assigns), do: "Internal Server Error"
+  def render("400.html", _assigns), do: "Bad Request"
+  def render("404.html", _assigns), do: "Not Found"
 
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
--- test/ping_web/views/error_view_test.exs
+++ test/ping_web/views/error_view_test.exs
@@ -4,6 +4,10 @@ defmodule PingWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  test "renders 400.html" do
+    assert render_to_string(PingWeb.ErrorView, "400.html", []) == "Bad Request"
+  end
+
   test "renders 404.html" do
     assert render_to_string(PingWeb.ErrorView, "404.html", []) == "Not Found"
   end
